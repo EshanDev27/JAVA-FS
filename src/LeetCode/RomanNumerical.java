02b0806182271d2de73b05d0b4b7a90a4556ba48
Eshan <laleshan7@gmail.com>
2024-01-16 19:47:53 +0530

    new

--- src/LeetCode/RomanNumerical.java
+++ src/LeetCode/RomanNumerical.java
@@ -5,7 +5,6 @@ import java.util.HashMap;
 public class RomanNumerical {
     public static void main(String[] args) {
         System.out.println(romtonum("IX"));
-        return;
     }
 
     static int romtonum(String str){
